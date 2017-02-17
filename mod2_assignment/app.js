4015455ae6801aeef132eb9d8b1239c11e2702e4
Cesar Martinez <Cesar Martinez>
2017-02-17 22:00:57 +0200

    Main functionalities

---
+++ mod2_assignment/app.js
@@ -0,0 +1,63 @@
+(function () {
+'use strict';
+
+var toBuyItems = [
+    { item_name: "Jar", item_quantity: 1 },
+    { item_name: "Cookies", item_quantity: 8 },
+    { item_name: "Monsters", item_quantity: 6 },
+    { item_name: "Sodlid", item_quantity: 5 },
+    { item_name: "Stuffs", item_quantity: 3 }
+  ];
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var showList = this;
+
+  showList.items = ShoppingListCheckOffService.getItems();
+
+  showList.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtItems = this;
+
+  boughtItems.boughtItemsList = ShoppingListCheckOffService.getBoughtItems();
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var items = toBuyItems;
+
+  //List of bought items
+  var boughtItemsList = [];
+
+  service.removeItem = function (itemIndex) {
+    var removedItem = items.splice(itemIndex, 1);
+    // console.log('removedItem', removedItem);
+    boughtItemsList.push(removedItem.pop());
+    // console.log('boughtItems', boughtItemsList);
+  };
+
+  service.getItems = function () {
+
+    return items;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItemsList;
+  };
+
+}
+
+})();
